f2b885e09ff352a42439bd65dedf32f50f934d53
Logwriter <rodrigo.p.nasc@gmail.com>
2016-12-07 18:09:13 -0500

    Fix LUN size formula

--- netapp-fast-deploy.py
+++ netapp-fast-deploy.py
@@ -206,7 +206,7 @@ def deploy(cli_options):
                         else:
                             lun_spec['path'] = '/vol/'+vol_spec['volume']+'/'+doc_vol['luns']['lun-name']+str(dl)
 
-                        lun_spec['size'] = int((vol_spec['size'] - 1)/luns_per_vol)
+                        lun_spec['size'] = int((vol_spec['size'] - 1073741824)/luns_per_vol)
                         lun_spec['ostype'] = doc_vol['luns']['os-type']
                         lun_spec['space-reservation-enabled'] = doc_vol['luns']['space-reservation-enabled']
                         lun_spec['space-allocation-enabled'] = doc_vol['luns']['space-allocation-enabled']
